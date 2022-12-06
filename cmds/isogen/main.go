0f96af1fc8e13b4a2628bdfc74d7a0e052eb63b7
Philipp Deppenwiese <zaolin.daisuki@gmail.com>
2022-12-06 15:25:11 +0100

    Join functions

--- cmds/isogen/main.go
+++ cmds/isogen/main.go
@@ -14,23 +14,11 @@ func main() {
 	mmx := flag.String("mmx", "", "a string")
 	out := flag.String("out", "", "a string")
 	flag.Parse()
-	if len(os.Args) < 5 {
+	if len(os.Args) < 4 {
 		os.Exit(-1)
 	}
-	f, err := os.CreateTemp("/var/tmp", "immune.*.vfat")
-	if err != nil {
-		fmt.Printf("failed to create temporary vfat file: %v", err)
-		os.Exit(-1)
-	}
-	f.Close()
-	os.RemoveAll(f.Name())
-
-	if err := isogen.Mkvfat(f.Name(), *linuxboot, *shim, *mmx); err != nil {
-		fmt.Printf("failed to make vfat partition: %v", err)
-		os.Exit(-1)
-	}
-	if err := isogen.Mkiso(*out, f.Name()); err != nil {
-		fmt.Printf("failed to make iso: %v", err)
+	if err := isogen.MkEFIBootloader(*out, *linuxboot, *shim, *mmx); err != nil {
+		fmt.Printf("failed to make efi bootloader iso: %v", err)
 		os.Exit(-1)
 	}
 }
--- pkg/iso.go
+++ pkg/iso.go
@@ -39,7 +39,7 @@ func writeFilesToFs(fs filesystem.FileSystem, file, diskPath string) error {
 	return nil
 }
 
-func Mkvfat(out, linuxBootFilepath, shimFilepath, mmxFilepath string) error {
+func MkEFIBootloader(out, linuxBootFilepath, shimFilepath, mmxFilepath string) error {
 	var espSize int64
 	for _, file := range []string{shimFilepath, linuxBootFilepath} {
 		if file != "" {
@@ -52,24 +52,35 @@ func Mkvfat(out, linuxBootFilepath, shimFilepath, mmxFilepath string) error {
 	}
 
 	var (
+		padding          int64  = 5 * 1024 * 1024
 		align1MiBMask    uint64 = (1<<44 - 1) << 20
 		partSize         int64  = int64(uint64(espSize) & align1MiBMask)
-		diskSize         int64  = partSize + 5*1024*1024
-		partitionStart   int64  = 2048
+		diskSize         int64  = (partSize * 2) + padding
 		partitionSectors int64  = partSize / int64(diskfs.SectorSize512)
-		partitionEnd     int64  = partitionSectors - partitionStart + 1
+		partition1Start  int64  = 2048
+		partition1End    int64  = partitionSectors - partition1Start + 1
+		partition2Start  int64  = partition1End + 1
+		partition2End    int64  = ((diskSize - padding) / int64(diskfs.SectorSize512)) - partition2Start + 1
 	)
 
 	disk, err := diskfs.Create(out, diskSize, diskfs.Raw, diskfs.SectorSize512)
 	if err != nil {
 		return fmt.Errorf("failed to create disk file: %w", err)
 	}
+	disk.LogicalBlocksize = 2048
 	table := &gpt.Table{
-		Partitions: make([]*gpt.Partition, 1),
+		Partitions: make([]*gpt.Partition, 2),
 	}
+	// TODO Attributes:
 	table.Partitions[0] = &gpt.Partition{
-		Start: uint64(partitionStart),
-		End:   uint64(partitionEnd),
+		Start: uint64(partition1Start),
+		End:   uint64(partition1End),
+		Type:  gpt.MicrosoftBasicData,
+		Name:  "ISO9660",
+	}
+	table.Partitions[1] = &gpt.Partition{
+		Start: uint64(partition2Start),
+		End:   uint64(partition2End),
 		Type:  gpt.EFISystemPartition,
 		Name:  "EFI System",
 	}
@@ -77,67 +88,53 @@ func Mkvfat(out, linuxBootFilepath, shimFilepath, mmxFilepath string) error {
 	if err != nil {
 		return fmt.Errorf("failed to create partitiont table: %w", err)
 	}
-	spec := diskpkg.FilesystemSpec{Partition: 0, FSType: filesystem.TypeFat32}
-	fs, err := disk.CreateFilesystem(spec)
+	iso := diskpkg.FilesystemSpec{Partition: 0, FSType: filesystem.TypeISO9660, VolumeLabel: volumeLabel}
+	part1, err := disk.CreateFilesystem(iso)
 	if err != nil {
-		return fmt.Errorf("failed to create filesystem")
+		return fmt.Errorf("failed to create iso9660 filesystem")
 	}
-	if err := writeFilesToFs(fs, shimFilepath, "/EFI/boot/bootx64.efi"); err != nil {
-		return fmt.Errorf("failed to write shim: %w", err)
+	if err := writeFilesToFs(part1, shimFilepath, "/EFI/boot/bootx64.efi"); err != nil {
+		return fmt.Errorf("failed to write shim to iso: %w", err)
 	}
-	if err := writeFilesToFs(fs, mmxFilepath, "/EFI/boot/mmx64.efi"); err != nil {
-		return fmt.Errorf("failed to write mmx: %w", err)
+	if err := writeFilesToFs(part1, mmxFilepath, "/EFI/boot/mmx64.efi"); err != nil {
+		return fmt.Errorf("failed to write mmx to iso: %w", err)
 	}
-	if err := writeFilesToFs(fs, linuxBootFilepath, "/EFI/boot/linuxboot.efi"); err != nil {
-		return fmt.Errorf("failed to write linuxboot: %w", err)
+	if err := writeFilesToFs(part1, linuxBootFilepath, "/EFI/boot/linuxboot.efi"); err != nil {
+		return fmt.Errorf("failed to write linuxboot to iso: %w", err)
 	}
-	return nil
-}
-
-func Mkiso(out, vfat string) error {
-	fi, err := os.Stat(vfat)
-	if err != nil {
-		return err
-	}
-	size := fi.Size()
-	size = size + 5*1024*1024 // disk padding
-	iso, err := diskfs.Create(out, size, diskfs.Raw, diskfs.SectorSize512)
-	if err != nil {
-		return err
-	}
-	iso.LogicalBlocksize = 2048
-	fs, err := iso.CreateFilesystem(diskpkg.FilesystemSpec{
-		Partition:   0,
-		FSType:      filesystem.TypeISO9660,
-		VolumeLabel: volumeLabel,
-	})
-	if err != nil {
-		return err
-	}
-	// This avoids an issue where path.Base in go-diskfs gives us a sigsegv
-	vfatName := filepath.Join("vfat", filepath.Base(vfat))
-	if err := writeFilesToFs(fs, vfat, vfatName); err != nil {
-		return fmt.Errorf("failed to write file %s to ISO: %w", vfat, err)
-	}
-	diskImage, ok := fs.(*iso9660.FileSystem)
+	isoImage, ok := part1.(*iso9660.FileSystem)
 	if !ok {
 		return fmt.Errorf("not an iso9660 filesystem")
 	}
 	options := iso9660.FinalizeOptions{
 		VolumeIdentifier: volumeIdentifier,
 		ElTorito: &iso9660.ElTorito{
-			BootCatalog: "/boot.catalog",
+			BootCatalog: "/BOOT.CAT",
 			Entries: []*iso9660.ElToritoEntry{
 				{
 					Platform:  iso9660.EFI,
 					Emulation: iso9660.NoEmulation,
-					BootFile:  vfatName,
+					BootFile:  "/EFI/boot/bootx64.efi",
 				},
 			},
 		},
 	}
-	if err = diskImage.Finalize(options); err != nil {
+	if err = isoImage.Finalize(options); err != nil {
 		return err
+	}
+	fat := diskpkg.FilesystemSpec{Partition: 1, FSType: filesystem.TypeFat32}
+	part2, err := disk.CreateFilesystem(fat)
+	if err != nil {
+		return fmt.Errorf("failed to create vfat filesystem")
+	}
+	if err := writeFilesToFs(part2, shimFilepath, "/EFI/boot/bootx64.efi"); err != nil {
+		return fmt.Errorf("failed to write shim: %w", err)
+	}
+	if err := writeFilesToFs(part2, mmxFilepath, "/EFI/boot/mmx64.efi"); err != nil {
+		return fmt.Errorf("failed to write mmx: %w", err)
+	}
+	if err := writeFilesToFs(part2, linuxBootFilepath, "/EFI/boot/linuxboot.efi"); err != nil {
+		return fmt.Errorf("failed to write linuxboot: %w", err)
 	}
 	return nil
 }
